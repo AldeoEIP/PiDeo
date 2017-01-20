e54c809725c1c40b77dc514bc3493077e744db57
aloisdg <aloisdegouvello@live.fr>
2017-01-20 13:54:02 +0100

    Add picture and basic cheap chat

--- PiDeo/MainPage.xaml.cs
+++ PiDeo/MainPage.xaml.cs
@@ -15,16 +15,20 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace PiDeo
-{
+namespace PiDeo {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
+    public sealed partial class MainPage : Page {
+        public MainPage() {
+            this.InitializeComponent ();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+            var message = Message.Text;
+            if (string.IsNullOrWhiteSpace (Message.Text))
+                return;
+            Answer.Text = message;
         }
     }
 }
